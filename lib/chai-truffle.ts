26af4967a49f40af6bc7cb9df0d3fd9aca4e1fd2
Calvin Lau <calvinlauco@gmail.com>
2019-07-07 22:02:20 +0800

    ♻️ Reordered function

--- lib/chai-truffle.ts
+++ lib/chai-truffle.ts
@@ -21,107 +21,6 @@ export = (chai: any, utils: ChaiUse.Utils): void => {
     Assertion.addMethod(name, assertFn);
   };
 
-  method("evmSuccess", function(this: ChaiUse.Assertion) {
-    assertIsPromiseLike(this._obj);
-
-    const obj: Promise<any> = this._obj;
-    const derivedPromise = obj.then(
-      (result: any) => {
-        // Promise resolves to transaction response, if the assertion expect not
-        // to broadcast, then it should fail.
-        failNegatedAssertion(
-          this,
-          "expected transaction to fail in EVM, but it succeeded",
-        );
-        new Assertion(result).to.be.transactionResponse;
-      },
-      (err: Error) => {
-        // Promise rejects to error, if the assertion expect to broadcast,
-        // then it should fail.
-        failAssertion(
-          this,
-          "expected transaction to succeed in EVM, but it failed",
-          {
-            actual: err,
-          },
-        );
-      },
-    );
-
-    this.then = derivedPromise.then.bind(derivedPromise);
-    return this;
-  });
-
-  method("evmFail", function(
-    this: ChaiUse.Assertion,
-    expectedErrorMessage?: string,
-  ) {
-    assertIsPromiseLike(this._obj);
-
-    const obj: Promise<any> = this._obj;
-    const derivedPromise = obj.then(
-      (result: any) => {
-        // Promise resolves to transaction response, if the assertion expect to fail,
-        // then it should fail.
-        const failMessage = isNil(expectedErrorMessage)
-          ? "expected transaction to fail in EVM, but it succeeded"
-          : `expected transaction to fail in EVM because of ${expectedErrorMessage}, but it succeeded`;
-        failAssertion(this, failMessage, {
-          actual: result,
-        });
-
-        new Assertion(result).to.be.transactionResponse;
-      },
-      (err: Error) => {
-        if (isNil(expectedErrorMessage)) {
-          // Promise rejects to error, if the assertion expect not to fail,
-          // then it should fail.
-          failNegatedAssertion(
-            this,
-            "expected transaction to succeed in EVM, but it failed",
-            {
-              actual: err,
-            },
-          );
-        } else {
-          this.assert(
-            err.message.indexOf(expectedErrorMessage as string) !== -1,
-            `expected transaction to fail in EVM because of ${expectedErrorMessage}, but it failed of another reason`,
-            `expected transaction not to fail in EVM because of ${expectedErrorMessage}, but it was`,
-            expectedErrorMessage,
-            err,
-          );
-        }
-      },
-    );
-
-    this.then = derivedPromise.then.bind(derivedPromise);
-    return this;
-  });
-
-  method("evmOutOfGas", function(this: ChaiUse.Assertion) {
-    return this.to.evmFail("out of gas");
-  });
-
-  method("evmRevert", function(this: ChaiUse.Assertion) {
-    return this.to.evmFail("revert");
-  });
-
-  property("transactionResponse", function(
-    this: ChaiUse.Assertion,
-  ): ChaiUse.Assertion {
-    this.assert(
-      isTransactionResponse(this._obj),
-      "expected #{this} to be a Truffle TransactionResponse",
-      "expected #{this} not to be a Truffle TransactionResponse",
-    );
-
-    return this;
-  });
-
-  method("eventLength", eventLengthAssertFn);
-  method("eventLengthOf", eventLengthAssertFn);
-
   method("emitEvent", function(
     this: ChaiUse.Assertion,
     expectedEventName?: string,
@@ -148,6 +47,26 @@ export = (chai: any, utils: ChaiUse.Utils): void => {
 
     return this;
   });
+
+  const assertHasEventEmittedWithAssertion = (
+    assertion: ChaiUse.Assertion,
+  ): ChaiUse.Assertion => {
+    new Assertion(assertion._obj).to.be.transactionResponse;
+
+    const obj: Truffle.TransactionResponse = assertion._obj;
+    const logWithEventFound = obj.logs.find((log) => !isNil(log.event));
+    const hasEventEmitted = !!logWithEventFound;
+    const eventEmitted =
+      hasEventEmitted && (logWithEventFound as Truffle.TransactionLog).event;
+
+    assertion.assert(
+      hasEventEmitted,
+      "expected transaction to emit event, but none was emitted",
+      `expected transaction not to emit event, but event ${eventEmitted} was emitted`,
+    );
+
+    return assertion;
+  };
 
   method("emitEventAt", function(
     this: ChaiUse.Assertion,
@@ -185,48 +104,10 @@ export = (chai: any, utils: ChaiUse.Utils): void => {
     return this;
   });
 
-  method("withEventArgs", function(
-    this: ChaiUse.Assertion,
-    assertArgsFn: (args: Truffle.TransactionLogArgs) => boolean,
-  ): ChaiUse.Assertion {
-    if (!isEmitEventAsserted(this)) {
-      throw new Error(
-        "to assert event arguments the assertion must be asserted with emitEvent() or emitEventAt() first. i.e. expect(...).to.emitEvent(...).withEventArgs(...)",
-      );
-    }
-    if (isNegatedBeforeAssertEmitEvent(this)) {
-      throw new Error(
-        "expect(...).not.to.emitEvent(...).withEventArgs(...) pattern is not support. If you are asserting a transaction has emitted an event but not with the certain argument format, consider using expect(...).to.emitEvent(...).not.withEventArgs(...) instead",
-      );
-    }
-
-    const obj: Truffle.TransactionResponse = this._obj;
-    const eventLogPositionList = getEmitEventLogPositionList(this);
-
-    const firstMatchedEventLogIndex = eventLogPositionList[0];
-    const eventName = obj.logs[firstMatchedEventLogIndex].event;
-
-    const matchedEventLogs = eventLogPositionList.map(
-      (position) => obj.logs[position],
-    );
-
-    const errorMessagePrefix = `expected transaction to emit event ${eventName} with matching argument(s)`;
-    const negatedErrorMessage = `expected transaction to emit event ${eventName} but not with matching argument(s), but argument(s) match`;
-    assertEmitEventWithArgsFromMatchedEventLogs(
-      this,
-      matchedEventLogs,
-      assertArgsFn,
-      errorMessagePrefix,
-      negatedErrorMessage,
-    );
-
-    return this;
-  });
-
   method("emitEventWithArgs", function(
     this: ChaiUse.Assertion,
     expectedEventName: string,
-    assertArgsFn: (args: Truffle.TransactionLogArgs) => boolean,
+    assertArgsFn: AssertArgsFn,
   ) {
     new Assertion(this._obj).to.be.transactionResponse;
 
@@ -301,13 +182,129 @@ export = (chai: any, utils: ChaiUse.Utils): void => {
     return this;
   });
 
-  const assertIsPromiseLike = (value: any) => {
-    new Assertion(value).assert(
-      typeof value.then !== "undefined",
-      "expected #{this} to be a Promise",
-      "expected #{this} not to be a Promise",
+  method("eventLength", eventLengthAssertFn);
+  method("eventLengthOf", eventLengthAssertFn);
+
+  function eventLengthAssertFn(
+    this: ChaiUse.Assertion,
+    expectedLength: number,
+  ): ChaiUse.Assertion {
+    assertIsTransactionResponse(this._obj);
+
+    const actualEventLogLength = (this._obj as Truffle.TransactionResponse).logs
+      .length;
+    this.assert(
+      actualEventLogLength === expectedLength,
+      `expected transaction to emit ${expectedLength} event log(s), but ${actualEventLogLength} was emitted`,
+      `expected transaction not to emit ${expectedLength} event log(s)`,
+      expectedLength, // TODO
+      actualEventLogLength,
     );
+
+    return this;
+  }
+
+  const assertIsTransactionResponse = (value: any) => {
+    new Assertion(value).is.transactionResponse;
   };
+
+  method("evmFail", function(
+    this: ChaiUse.Assertion,
+    expectedErrorMessage?: string,
+  ) {
+    assertIsPromiseLike(this._obj);
+
+    const obj: Promise<any> = this._obj;
+    const derivedPromise = obj.then(
+      (result: any) => {
+        // Promise resolves to transaction response, if the assertion expect to fail,
+        // then it should fail.
+        const failMessage = isNil(expectedErrorMessage)
+          ? "expected transaction to fail in EVM, but it succeeded"
+          : `expected transaction to fail in EVM because of ${expectedErrorMessage}, but it succeeded`;
+        failAssertion(this, failMessage, {
+          actual: result,
+        });
+
+        new Assertion(result).to.be.transactionResponse;
+      },
+      (err: Error) => {
+        if (isNil(expectedErrorMessage)) {
+          // Promise rejects to error, if the assertion expect not to fail,
+          // then it should fail.
+          failNegatedAssertion(
+            this,
+            "expected transaction to succeed in EVM, but it failed",
+            {
+              actual: err,
+            },
+          );
+        } else {
+          this.assert(
+            err.message.indexOf(expectedErrorMessage as string) !== -1,
+            `expected transaction to fail in EVM because of ${expectedErrorMessage}, but it failed of another reason`,
+            `expected transaction not to fail in EVM because of ${expectedErrorMessage}, but it was`,
+            expectedErrorMessage,
+            err,
+          );
+        }
+      },
+    );
+
+    this.then = derivedPromise.then.bind(derivedPromise);
+    return this;
+  });
+
+  method("evmOutOfGas", function(this: ChaiUse.Assertion) {
+    return this.to.evmFail("out of gas");
+  });
+
+  method("evmRevert", function(this: ChaiUse.Assertion) {
+    return this.to.evmFail("revert");
+  });
+
+  method("evmSuccess", function(this: ChaiUse.Assertion) {
+    assertIsPromiseLike(this._obj);
+
+    const obj: Promise<any> = this._obj;
+    const derivedPromise = obj.then(
+      (result: any) => {
+        // Promise resolves to transaction response, if the assertion expect not
+        // to broadcast, then it should fail.
+        failNegatedAssertion(
+          this,
+          "expected transaction to fail in EVM, but it succeeded",
+        );
+        new Assertion(result).to.be.transactionResponse;
+      },
+      (err: Error) => {
+        // Promise rejects to error, if the assertion expect to broadcast,
+        // then it should fail.
+        failAssertion(
+          this,
+          "expected transaction to succeed in EVM, but it failed",
+          {
+            actual: err,
+          },
+        );
+      },
+    );
+
+    this.then = derivedPromise.then.bind(derivedPromise);
+    return this;
+  });
+
+  property("transactionResponse", function(
+    this: ChaiUse.Assertion,
+  ): ChaiUse.Assertion {
+    this.assert(
+      isTransactionResponse(this._obj),
+      "expected #{this} to be a Truffle TransactionResponse",
+      "expected #{this} not to be a Truffle TransactionResponse",
+    );
+
+    return this;
+  });
 
   const TRANSACTION_RESPONSE_KEYS = ["tx", "receipt", "logs"];
   const isTransactionResponse = (value: any): boolean => {
@@ -326,25 +323,43 @@ export = (chai: any, utils: ChaiUse.Utils): void => {
     return true;
   };
 
-  const assertHasEventEmittedWithAssertion = (
-    assertion: ChaiUse.Assertion,
-  ): ChaiUse.Assertion => {
-    new Assertion(assertion._obj).to.be.transactionResponse;
+  method("withEventArgs", function(
+    this: ChaiUse.Assertion,
+    assertArgsFn: (args: Truffle.TransactionLogArgs) => boolean,
+  ): ChaiUse.Assertion {
+    if (!isEmitEventAsserted(this)) {
+      throw new Error(
+        "to assert event arguments the assertion must be asserted with emitEvent() or emitEventAt() first. i.e. expect(...).to.emitEvent(...).withEventArgs(...)",
+      );
+    }
+    if (isNegatedBeforeAssertEmitEvent(this)) {
+      throw new Error(
+        "expect(...).not.to.emitEvent(...).withEventArgs(...) pattern is not support. If you are asserting a transaction has emitted an event but not with the certain argument format, consider using expect(...).to.emitEvent(...).not.withEventArgs(...) instead",
+      );
+    }
 
-    const obj: Truffle.TransactionResponse = assertion._obj;
-    const logWithEventFound = obj.logs.find((log) => !isNil(log.event));
-    const hasEventEmitted = !!logWithEventFound;
-    const eventEmitted =
-      hasEventEmitted && (logWithEventFound as Truffle.TransactionLog).event;
+    const obj: Truffle.TransactionResponse = this._obj;
+    const eventLogPositionList = getEmitEventLogPositionList(this);
 
-    assertion.assert(
-      hasEventEmitted,
-      "expected transaction to emit event, but none was emitted",
-      `expected transaction not to emit event, but event ${eventEmitted} was emitted`,
+    const firstMatchedEventLogIndex = eventLogPositionList[0];
+    const eventName = obj.logs[firstMatchedEventLogIndex].event;
+
+    const matchedEventLogs = eventLogPositionList.map(
+      (position) => obj.logs[position],
     );
 
-    return assertion;
-  };
+    const errorMessagePrefix = `expected transaction to emit event ${eventName} with matching argument(s)`;
+    const negatedErrorMessage = `expected transaction to emit event ${eventName} but not with matching argument(s), but argument(s) match`;
+    assertEmitEventWithArgsFromMatchedEventLogs(
+      this,
+      matchedEventLogs,
+      assertArgsFn,
+      errorMessagePrefix,
+      negatedErrorMessage,
+    );
+
+    return this;
+  });
 
   const assertEmitEventWithArgsFromMatchedEventLogs = (
     assertion: ChaiUse.Assertion,
@@ -388,6 +403,30 @@ export = (chai: any, utils: ChaiUse.Utils): void => {
     );
   };
 
+  const assertIsPromiseLike = (value: any) => {
+    new Assertion(value).assert(
+      typeof value.then !== "undefined",
+      "expected #{this} to be a Promise",
+      "expected #{this} not to be a Promise",
+    );
+  };
+
+  const failNegatedAssertion = (
+    assertion: ChaiUse.Assertion,
+    message: string,
+    value: FailAssertionValue = {},
+  ) => {
+    assertion.assert(true, "", message, value.expected, value.actual);
+  };
+
+  const failAssertion = (
+    assertion: ChaiUse.Assertion,
+    message: string,
+    value: FailAssertionValue = {},
+  ) => {
+    assertion.assert(false, message, "", value.expected, value.actual);
+  };
+
   const isNegated = (assertion: ChaiUse.Assertion): boolean => {
     return !isNil(utils.flag(assertion, "negate"));
   };
@@ -420,49 +459,6 @@ export = (chai: any, utils: ChaiUse.Utils): void => {
   ): number[] => {
     return utils.flag(assertion, "truffleEmitEventLogPositionList");
   };
-
-  function eventLengthAssertFn(
-    this: ChaiUse.Assertion,
-    expectedLength: number,
-  ): ChaiUse.Assertion {
-    assertIsTransactionResponse(this._obj);
-
-    const actualEventLogLength = (this._obj as Truffle.TransactionResponse).logs
-      .length;
-    this.assert(
-      actualEventLogLength === expectedLength,
-      `expected transaction to emit ${expectedLength} event log(s), but ${actualEventLogLength} was emitted`,
-      `expected transaction not to emit ${expectedLength} event log(s)`,
-      expectedLength, // TODO
-      actualEventLogLength,
-    );
-
-    return this;
-  }
-
-  /**
-   *
-   * @param assertion Chai.use Assertion
-   * @param value The
-   */
-  const assertIsTransactionResponse = (value: any) => {
-    new Assertion(value).is.transactionResponse;
-  };
-};
-
-const failNegatedAssertion = (
-  assertion: ChaiUse.Assertion,
-  message: string,
-  value: FailAssertionValue = {},
-) => {
-  assertion.assert(true, "", message, value.expected, value.actual);
-};
-const failAssertion = (
-  assertion: ChaiUse.Assertion,
-  message: string,
-  value: FailAssertionValue = {},
-) => {
-  assertion.assert(false, message, "", value.expected, value.actual);
 };
 
 type AssertArgsFn = (args: Truffle.TransactionLogArgs) => boolean;
